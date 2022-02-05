5cfe2e31bf1c83cc7d7a7514a6ec2cfb74e196d6
IvanMOA <ax3l.imo@gmail.com>
2022-02-05 11:33:27 -0600

    Se agrega filtro fuzzy finder para la lista de filtros

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,32 @@
 import { Checkbox } from '../Checkbox';
+import { useForm } from 'react-hook-form';
+import { Input } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { matchSorter } from 'match-sorter';
 
 export const Filter = ({ control, filterableValues }) => {
+  const [filteredFilterableValues, setFilteredFilterableValues] = useState([]);
+  const { register, watch } = useForm({
+    defaultValues: {
+      search: '',
+    },
+  });
+  const searchTerm = watch('search');
+  useEffect(() => {
+    setFilteredFilterableValues(matchSorter(filterableValues, searchTerm));
+  }, [searchTerm]);
+  const isSearchTermEmpty = searchTerm === '';
   return (
     <div>
+      <Input placeholder="Search" {...register('search')} />
       {filterableValues.map((value) => (
-        <Checkbox key={value} control={control} name={value} />
+        <div
+          className={
+            !isSearchTermEmpty && !filteredFilterableValues.includes(value) ? 'hidden' : 'aw'
+          }
+        >
+          <Checkbox key={value} control={control} name={value} />
+        </div>
       ))}
     </div>
   );
