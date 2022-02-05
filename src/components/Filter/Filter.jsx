12cad5d5acc193ec6a31750cbbd3493f6d84d2be
IvanMOA <ax3l.imo@gmail.com>
2022-02-05 11:44:22 -0600

    Se mueve filtro a dropdown

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,10 @@
 import { Checkbox } from '../Checkbox';
 import { useForm } from 'react-hook-form';
-import { Input } from '@chakra-ui/react';
+import { Input, Menu, MenuButton, MenuList } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { matchSorter } from 'match-sorter';
-
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import clsx from 'clsx';
 export const Filter = ({ control, filterableValues }) => {
   const [filteredFilterableValues, setFilteredFilterableValues] = useState([]);
   const { register, watch } = useForm({
@@ -18,16 +19,33 @@ export const Filter = ({ control, filterableValues }) => {
   const isSearchTermEmpty = searchTerm === '';
   return (
     <div>
-      <Input placeholder="Search" {...register('search')} />
-      {filterableValues.map((value) => (
-        <div
-          className={
-            !isSearchTermEmpty && !filteredFilterableValues.includes(value) ? 'hidden' : 'aw'
-          }
-        >
-          <Checkbox key={value} control={control} name={value} />
-        </div>
-      ))}
+      <Menu>
+        <MenuButton className="hover:bg-gray-100 rounded-md px-2 py-1">
+          Filtros <ChevronDownIcon />
+        </MenuButton>
+        <MenuList>
+          <div className="px-3 py-1">
+            <Input
+              className="mb-2"
+              size="sm"
+              autocomplete="off"
+              autofill="off"
+              placeholder="Search"
+              {...register('search')}
+            />
+            {filterableValues.map((value) => (
+              <div
+                key={value}
+                className={clsx('text-gray-600', {
+                  hidden: !isSearchTermEmpty && !filteredFilterableValues.includes(value),
+                })}
+              >
+                <Checkbox control={control} name={value} />
+              </div>
+            ))}
+          </div>
+        </MenuList>
+      </Menu>
     </div>
   );
 };
